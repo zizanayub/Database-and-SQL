3520fd4cdccb52464dbcbb6ee5e01ac2724eb037
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-04-29 01:41:09 +0600

    Update 2-9-Temp Tables (2 insights).sql

--- 2-9-Temp Tables (2 insights).sql
+++ 2-9-Temp Tables (2 insights).sql
@@ -54,3 +54,29 @@ FROM EmployeeSalary
  -- ISSUE --
  /* In SQLite, temp files don't work properly. You should select all the previous queries to get the result. What's the reason? 
  */ 
+ 
+ 
+ 
+ -- Drop Table -- 
+
+DROP TABLE IF EXISTS #temp_employee_2 
+CREATE TABLE #temp_employee_2 (
+   jobtitle varchar(50),
+   EmployeePerJob int,
+   AverageAge int,
+   AverageSalary int)
+   
+ 
+ INSERT Into #temp_employee_2 
+ SELECT jobtitle, count(jobtitle), Avg(Age), Avg(Salary)
+ FROM EmployeeDemographics emp 
+ JOIN EmployeeSalary sal 
+ ON emp.employeeID = sal.EmployeeID 
+ GROUP BY jobtitle
+ 
+ SELECT *
+ FROM #temp_employee_2 
+
+
+/* By this query, if this table exists, this query will clear the table and work again. 
+If doesn't exist, it will make another new similar one table */ 
